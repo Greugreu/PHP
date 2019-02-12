83040904e804f562d963c28d9a27d5ea5d8a5b85
Victor Clarke <fenris.fitz@hotmail.fr>
2019-02-12 14:10:55 +0100

    add header.php

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=accueil">Menu 1</a></li>
+            <li><a href="index.php?page=produits">Menu 2</a></li>
+            <li><a href="index.php?page=inscription">Menu 3</a></li>
+            <li><a href="index.php?page=contact">Menu 4</a></li>
+        </ul>
+    </nav>
+</header>
