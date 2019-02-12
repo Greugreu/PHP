e9e2a275d895c81d92b1ba14fd2f92a585889e2e
Victor Clarke <fenris.fitz@hotmail.fr>
2019-02-12 12:10:43 +0100

    AMAZING

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+$message = "<h1>Je fais du PHP - boucle <em>for</em></h1>";
+$message .= "<ul>";
+    for($i = 0; $i < 11; $i++){
+        $message .= "<li>" . $i . "</li>";
+    }
+$message .= "</ul>";
+echo $message;
